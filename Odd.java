27c3fa3d7fc04ee01f6e54866092057fb0b59cdf
P.Raman <41136876+pramanram@users.noreply.github.com>
2018-07-21 11:45:25 +0530

    Add files via upload

---
+++ Odd.java
@@ -0,0 +1,15 @@
+package guvi1;
+import java.util.*;
+public class Odd {
+	public static void main(String[] args) {
+		int n;
+		Scanner in=new Scanner(System.in);
+		n=in.nextInt();
+		if(n%2==0){
+			System.out.println("Even");
+		}else{
+			System.out.println("Odd");
+		}
+	}
+
+}
