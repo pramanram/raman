edd74c90a38988493d1c5ff0eaeb2668aef29c09
P.Raman <41136876+pramanram@users.noreply.github.com>
2018-07-21 11:31:46 +0530

    Create Positive.java

---
+++ Positive.java
@@ -0,0 +1,17 @@
+package guvi1;
+import java.util.*;
+public class Positive {
+	public static void main(String[] args) {
+		int n;
+		Scanner in=new Scanner(System.in);
+		n=in.nextInt();
+		if(n==0){
+			System.out.println("Zero");
+		}else if(n>0){
+			System.out.println("Positive");
+		}else{
+			System.out.println("Negative");
+		}
+	}
+
+} 
